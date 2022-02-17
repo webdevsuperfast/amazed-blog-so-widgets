a1531604e71cb289f7c0ccb78e7c1b2d445263a3
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-02-17 09:29:11 +0800

    Added layout & class changes

--- widgets/amazed-blog-categories/tpl/default.php
+++ widgets/amazed-blog-categories/tpl/default.php
@@ -9,12 +9,12 @@ $widget_id = preg_replace( '/[^0-9]/', '', $widget_id );
 $attributes = array();
 
 $classes = array();
-$classes[] = 'amazed-blog-posts';
+$classes[] = 'amazed-blog-categories';
 $classes[] = $class;
 
 $attributes = array(
     'class' => esc_attr( implode( ' ', $classes ) ),
-    'id' => 'amazed-blog-posts-' . (int)$widget_id,
+    'id' => 'amazed-blog-categories-' . (int)$widget_id,
     'data-instance' => (int)$widget_id
 ); ?>
 
@@ -26,7 +26,40 @@ $attributes = array(
 <?php if ( $categories ) : ?>
   <div <?php foreach( $attributes as $name => $value ) echo $name . '="' . $value . '" ' ?>>
     <?php foreach( $categories as $category ) : ?>
-      <?php echo $category->name; ?>
+      <?php if ( function_exists( 'z_taxonomy_image_url' ) ) : ?>
+        <div class="category">
+          <?php if ( in_array( 'thumbnail', $display ) ) : ?>
+          <div class="category-image">
+            <?php 
+            if ( $size == 'custom_size' && ! empty( $instance['structure']['size_width'] ) && ! empty( $instance['structure']['size_height'] ) ) {
+              $size = array(
+                (int) $instance['structure']['size_width'],
+                (int) $instance['structure']['size_height']
+              );
+            }
+            
+            if ( get_option( 'z_taxonomy_image' . $category->term_id ) ) :
+              echo '<img src=" '. z_taxonomy_image_url( $category->term_id, $size ) .'" alt="'. $category->name .'">';
+            endif; 
+            ?>
+          </div>
+          <?php endif; ?>
+
+          <?php if ( in_array( 'title', $display ) ) : ?>
+          <div class="category-title">
+            <a href="<?php echo get_term_link( $category->term_id ); ?>">
+              <?php echo $category->name; ?>
+            </a>
+          </div>
+          <?php endif; ?>
+
+          <?php if ( in_array( 'content', $display ) ) : ?>
+            <div class="category-info">
+              <p class="category-count"><?php echo $category->count; ?></p>
+            </div>
+          <?php endif; ?>
+        </div>
+      <?php endif; ?>
     <?php endforeach; ?>
   </div>
 <?php endif; ?>
