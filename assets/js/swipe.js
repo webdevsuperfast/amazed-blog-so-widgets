5ac40c13e0ee948f4154b00b99f792feee286572
czar.gaba1 <czar.gaba@appetiser.com.au>
2022-02-23 17:35:11 +0800

    Added JS files

---
+++ assets/js/swipe.js
@@ -0,0 +1,17 @@
+import Swiper, { Navigation } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+let slider = document.getElementsByClassName("swiper");
+
+for (var i = 0; i < slider.length; i++) {
+  // console.log(slider[i].dataset.instance);
+  const mySwiper = new Swiper(slider[i], {
+    // const swiper = new Swiper(".swiper-" + i.grid.dataset.instance, {
+    modules: [Navigation],
+    slidesPerView: 1,
+    // autoHeight: true,
+    spaceBetween: 40,
+  });
+}
