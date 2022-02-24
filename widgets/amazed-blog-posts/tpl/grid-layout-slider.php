c3b6243a0f6c6d858dc8b6d33d4a291f2ff442d7
webdevsuperfast <acob.rotsen@hotmail.com>
2022-02-24 15:56:20 +0800

    Fixed: Broken layout when disabling slider

--- widgets/amazed-blog-posts/tpl/grid-layout-slider.php
+++ widgets/amazed-blog-posts/tpl/grid-layout-slider.php
@@ -59,7 +59,7 @@ $grid = 2;
 $loop = new WP_Query( $post_args ); ?>
 <div <?php foreach( $attributes as $name => $value ) echo $name . '="' . $value . '" ' ?>>
 <?php echo $slider_enable ? '<div class="swiper-wrapper">' : ''; ?>
-<?php echo $slider_enable ? '<div class="swiper-slide absw-grid absw-grid-cols-none sm:absw-grid-rows-4 sm:absw-grid-cols-3 md:absw-grid-cols-10 lg:absw-grid-cols-20 absw-gap-8">' : '<div class="absw-grid absw-grid-cols-none sm:absw-grid-rows-4 sm:absw-grid-cols-3 md:absw-grid-cols-10 lg:absw-grid-cols-20 absw-gap-8">'; ?>
+<?php echo $slider_enable ? '<div class="swiper-slide absw-grid absw-grid-cols-none sm:absw-grid-rows-4 sm:absw-grid-cols-3 md:absw-grid-cols-10 lg:absw-grid-cols-20 absw-gap-8">' : ''; ?>
   <?php if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
   <?php if ( $i % 5 == 1 ) {
     $class = 'post-featured sm:absw-col-span-2 md:absw-col-span-6 lg:absw-col-span-11 sm:absw-row-span-full';
@@ -118,7 +118,11 @@ $loop = new WP_Query( $post_args ); ?>
     </div>
     <?php if ( $i % 5 == 0 ) { ?>
       </div><!-- end every 5th post -->
-      <?php echo $slider_enable ? '<div class="swiper-slide absw-grid absw-grid-cols-none sm:absw-grid-rows-4 sm:absw-grid-cols-3 md:absw-grid-cols-10 lg:absw-grid-cols-20 absw-gap-8">' : '<div class="absw-grid absw-grid-cols-none sm:absw-grid-rows-4 sm:absw-grid-cols-3 md:absw-grid-cols-10 lg:absw-grid-cols-20 absw-gap-8">'
+      <?php if ( $slider_enable ) { ?>
+        <div class="swiper-slide absw-grid absw-grid-cols-none sm:absw-grid-rows-4 sm:absw-grid-cols-3 md:absw-grid-cols-10 lg:absw-grid-cols-20 absw-gap-8">
+      <?php } else { ?>
+        <div <?php foreach( $attributes as $name => $value ) echo $name . '="' . $value . '" ' ?>>
+      <?php } ?>
     <?php } ?>
   <?php $i++; endwhile; wp_reset_query(); endif; ?>
 </div><!-- end every 5th post -->
