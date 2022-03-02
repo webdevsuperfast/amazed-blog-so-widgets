f7f1f95a226d70de9329ec429c1a073f08e3231d
czarg <czargabadeveloper@gmail.com>
2022-03-02 16:27:52 +0800

    add rawb_thumb_sizes function

--- amazed-blog-so-widgets.php
+++ amazed-blog-so-widgets.php
@@ -66,4 +66,27 @@ $active['rawb-image-carousel'] = true;
 	}
 }
 
+
+
+
+// Get an array of registered images
+function rawb_thumb_sizes() {
+	global $_wp_additional_image_sizes;
+ 
+    $sizes = array(
+		'full' => __( 'Full', 'ra-widgets-bundle' )
+	);
+
+	$get_intermediate_image_sizes = get_intermediate_image_sizes();
+
+	
+	
+	foreach( $get_intermediate_image_sizes as $_size ) {
+		if ( in_array( $_size, array( 'thumbnail', 'medium', 'large' ) ) ) {
+			$sizes[ $_size ] = ucwords( $_size ); // strtouppercase
+		}
+	}
+
+	return $sizes;
+}
 new ABSW_Widgets();
