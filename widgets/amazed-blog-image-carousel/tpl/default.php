2a4c6045959fa1dd77892b492f93641a48987618
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-03-09 13:22:39 +0800

    Added navigation markup

--- widgets/amazed-blog-image-carousel/tpl/default.php
+++ widgets/amazed-blog-image-carousel/tpl/default.php
@@ -2,8 +2,27 @@
 $widget_id = $args['widget_id'];
 $widget_id = preg_replace( '/[^0-9]/', '', $widget_id );
 
+$slider_navigation = '';
+$slider_navigation .= '<div class="slider-navigation absw-flex">';
+
+$slider_navigation .= '<div class="swiper-prev-'. (int) $widget_id .'">'. apply_filters( 'absw_image_carousel_prev_text' , 'Prev' ) .'</div>';
+$slider_navigation .= '<div class="swiper-next-'. (int) $widget_id .'">'. apply_filters( 'absw_image_carousel_next_text' , 'Next' ) .'</div>';
+
+$slider_navigation .= '</div>';
+
+if ( ! $slider_enable ) {
+  $slider_navigation = '';
+}
+
+if ( ! $slider_navigation ) {
+  $slider_navigation = '';
+}
+
+$before_title = preg_replace( '/<h3(.*?)>/', '<div class="absw-flex absw-flex-nowrap absw-items-center"><h3 class="absw-grow widget-title">', $args['before_title'] );
+$after_title = preg_replace( '/<\/h3>/', '</h3>' . $slider_navigation . '</div>', $args['after_title'] );
+
 if ( $title ) {
-  echo $args['before_title'] . apply_filters( 'widget_title', $title ) . $args['after_title'];
+  echo $before_title . apply_filters( 'widget_title', $title ) . $after_title;
 }
 
 $attributes = array();
